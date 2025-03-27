e8c93305f721723cc66739f5fa5b46161bcb7a75
Simon Knott <info@simonknott.de>
2025-03-27 20:48:02 +0100

    chore: refactor tests

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -19,193 +19,118 @@ import path from 'node:path';
 import { test, expect } from './fixtures';
 
 test('test tool list', async ({ server, visionServer }) => {
-  const list = await server.send({
-    jsonrpc: '2.0',
-    id: 1,
-    method: 'tools/list',
-  });
-
-  expect(list).toEqual(expect.objectContaining({
-    id: 1,
-    result: expect.objectContaining({
-      tools: [
-        expect.objectContaining({
-          name: 'browser_navigate',
-        }),
-        expect.objectContaining({
-          name: 'browser_go_back',
-        }),
-        expect.objectContaining({
-          name: 'browser_go_forward',
-        }),
-        expect.objectContaining({
-          name: 'browser_snapshot',
-        }),
-        expect.objectContaining({
-          name: 'browser_click',
-        }),
-        expect.objectContaining({
-          name: 'browser_hover',
-        }),
-        expect.objectContaining({
-          name: 'browser_type',
-        }),
-        expect.objectContaining({
-          name: 'browser_select_option',
-        }),
-        expect.objectContaining({
-          name: 'browser_take_screenshot',
-        }),
-        expect.objectContaining({
-          name: 'browser_press_key',
-        }),
-        expect.objectContaining({
-          name: 'browser_wait',
-        }),
-        expect.objectContaining({
-          name: 'browser_save_as_pdf',
-        }),
-        expect.objectContaining({
-          name: 'browser_close',
-        }),
-      ],
+  expect(await server.listTools()).toEqual([
+    expect.objectContaining({
+      name: 'browser_navigate',
     }),
-  }));
-
-  const visionList = await visionServer.send({
-    jsonrpc: '2.0',
-    id: 1,
-    method: 'tools/list',
-  });
-
-  expect(visionList).toEqual(expect.objectContaining({
-    id: 1,
-    result: expect.objectContaining({
-      tools: expect.arrayContaining([
-        expect.objectContaining({
-          name: 'browser_navigate',
-        }),
-        expect.objectContaining({
-          name: 'browser_go_back',
-        }),
-        expect.objectContaining({
-          name: 'browser_go_forward',
-        }),
-        expect.objectContaining({
-          name: 'browser_screenshot',
-        }),
-        expect.objectContaining({
-          name: 'browser_move_mouse',
-        }),
-        expect.objectContaining({
-          name: 'browser_click',
-        }),
-        expect.objectContaining({
-          name: 'browser_drag',
-        }),
-        expect.objectContaining({
-          name: 'browser_type',
-        }),
-        expect.objectContaining({
-          name: 'browser_press_key',
-        }),
-        expect.objectContaining({
-          name: 'browser_wait',
-        }),
-        expect.objectContaining({
-          name: 'browser_save_as_pdf',
-        }),
-        expect.objectContaining({
-          name: 'browser_close',
-        }),
-      ]),
+    expect.objectContaining({
+      name: 'browser_go_back',
     }),
-  }));
+    expect.objectContaining({
+      name: 'browser_go_forward',
+    }),
+    expect.objectContaining({
+      name: 'browser_snapshot',
+    }),
+    expect.objectContaining({
+      name: 'browser_click',
+    }),
+    expect.objectContaining({
+      name: 'browser_hover',
+    }),
+    expect.objectContaining({
+      name: 'browser_type',
+    }),
+    expect.objectContaining({
+      name: 'browser_select_option',
+    }),
+    expect.objectContaining({
+      name: 'browser_take_screenshot',
+    }),
+    expect.objectContaining({
+      name: 'browser_press_key',
+    }),
+    expect.objectContaining({
+      name: 'browser_wait',
+    }),
+    expect.objectContaining({
+      name: 'browser_save_as_pdf',
+    }),
+    expect.objectContaining({
+      name: 'browser_close',
+    }),
+  ]);
+
+  expect(await visionServer.listTools()).toEqual([
+    expect.objectContaining({
+      name: 'browser_navigate',
+    }),
+    expect.objectContaining({
+      name: 'browser_go_back',
+    }),
+    expect.objectContaining({
+      name: 'browser_go_forward',
+    }),
+    expect.objectContaining({
+      name: 'browser_screenshot',
+    }),
+    expect.objectContaining({
+      name: 'browser_move_mouse',
+    }),
+    expect.objectContaining({
+      name: 'browser_click',
+    }),
+    expect.objectContaining({
+      name: 'browser_drag',
+    }),
+    expect.objectContaining({
+      name: 'browser_type',
+    }),
+    expect.objectContaining({
+      name: 'browser_press_key',
+    }),
+    expect.objectContaining({
+      name: 'browser_wait',
+    }),
+    expect.objectContaining({
+      name: 'browser_save_as_pdf',
+    }),
+    expect.objectContaining({
+      name: 'browser_close',
+    }),
+  ]);
 });
 
 test('test resources list', async ({ server }) => {
-  const list = await server.send({
-    jsonrpc: '2.0',
-    id: 2,
-    method: 'resources/list',
-  });
-
-  expect(list).toEqual(expect.objectContaining({
-    id: 2,
-    result: expect.objectContaining({
-      resources: [
-        expect.objectContaining({
-          uri: 'browser://console',
-          mimeType: 'text/plain',
-        }),
-      ],
+  expect(await server.listResources()).toEqual([
+    expect.objectContaining({
+      uri: 'browser://console',
+      mimeType: 'text/plain',
     }),
-  }));
+  ]);
 });
 
 test('test browser_navigate', async ({ server }) => {
-  const response = await server.send({
-    jsonrpc: '2.0',
-    id: 2,
-    method: 'tools/call',
-    params: {
-      name: 'browser_navigate',
-      arguments: {
-        url: 'data:text/html,<html><title>Title</title><body>Hello, world!</body></html>',
-      },
-    },
-  });
-
-  expect(response).toEqual(expect.objectContaining({
-    id: 2,
-    result: {
-      content: [{
-        type: 'text',
-        text: `
+  expect(await server.callTool('browser_navigate', { url: 'data:text/html,<html><title>Title</title><body>Hello, world!</body></html>' })).toEqual([
+    `
 - Page URL: data:text/html,<html><title>Title</title><body>Hello, world!</body></html>
 - Page Title: Title
 - Page Snapshot
 \`\`\`yaml
 - document [ref=s1e2]: Hello, world!
 \`\`\`
-`,
-      }],
-    },
-  }));
+`
+  ]);
 });
 
 test('test browser_click', async ({ server }) => {
-  await server.send({
-    jsonrpc: '2.0',
-    id: 2,
-    method: 'tools/call',
-    params: {
-      name: 'browser_navigate',
-      arguments: {
-        url: 'data:text/html,<html><title>Title</title><button>Submit</button></html>',
-      },
-    },
-  });
+  await server.callTool(
+      'browser_navigate',
+      { url: 'data:text/html,<html><title>Title</title><button>Submit</button></html>' }
+  );
 
-  const response = await server.send({
-    jsonrpc: '2.0',
-    id: 3,
-    method: 'tools/call',
-    params: {
-      name: 'browser_click',
-      arguments: {
-        element: 'Submit button',
-        ref: 's1e4',
-      },
-    },
-  });
-
-  expect(response).toEqual(expect.objectContaining({
-    id: 3,
-    result: {
-      content: [{
-        type: 'text',
-        text: `\"Submit button\" clicked
+  expect(await server.callTool('browser_click', { element: 'Submit button', ref: 's1e4' })).toEqual([
+    `\"Submit button\" clicked
 
 - Page URL: data:text/html,<html><title>Title</title><button>Submit</button></html>
 - Page Title: Title
@@ -214,112 +139,37 @@ test('test browser_click', async ({ server }) => {
 - document [ref=s2e2]:
   - button \"Submit\" [ref=s2e4]
 \`\`\`
-`,
-      }],
-    },
-  }));
+`]);
 });
 
 test('test reopen browser', async ({ server }) => {
-  const response2 = await server.send({
-    jsonrpc: '2.0',
-    id: 2,
-    method: 'tools/call',
-    params: {
-      name: 'browser_navigate',
-      arguments: {
-        url: 'data:text/html,<html><title>Title</title><body>Hello, world!</body></html>',
-      },
-    },
-  });
+  await server.callTool(
+      'browser_navigate',
+      { url: 'data:text/html,<html><title>Title</title><body>Hello, world!</body></html>' }
+  );
 
-  expect(response2).toEqual(expect.objectContaining({
-    id: 2,
-  }));
+  expect(await server.callTool('browser_close')).toEqual(['Page closed']);
 
-  const response3 = await server.send({
-    jsonrpc: '2.0',
-    id: 3,
-    method: 'tools/call',
-    params: {
-      name: 'browser_close',
-    },
-  });
-
-  expect(response3).toEqual(expect.objectContaining({
-    id: 3,
-    result: {
-      content: [{
-        text: 'Page closed',
-        type: 'text',
-      }],
-    },
-  }));
-
-  const response4 = await server.send({
-    jsonrpc: '2.0',
-    id: 4,
-    method: 'tools/call',
-    params: {
-      name: 'browser_navigate',
-      arguments: {
-        url: 'data:text/html,<html><title>Title</title><body>Hello, world!</body></html>',
-      },
-    },
-  });
-
-  expect(response4).toEqual(expect.objectContaining({
-    id: 4,
-    result: {
-      content: [{
-        type: 'text',
-        text: `
+  expect(await server.callTool('browser_navigate', { url: 'data:text/html,<html><title>Title</title><body>Hello, world!</body></html>' })).toEqual([`
 - Page URL: data:text/html,<html><title>Title</title><body>Hello, world!</body></html>
 - Page Title: Title
 - Page Snapshot
 \`\`\`yaml
 - document [ref=s1e2]: Hello, world!
 \`\`\`
-`,
-      }],
-    },
-  }));
+`,]);
 });
 
 test.describe('test browser_select_option', () => {
   test('single option', async ({ server }) => {
-    await server.send({
-      jsonrpc: '2.0',
-      id: 2,
-      method: 'tools/call',
-      params: {
-        name: 'browser_navigate',
-        arguments: {
-          url: 'data:text/html,<html><title>Title</title><select><option value="foo">Foo</option><option value="bar">Bar</option></select></html>',
-        },
-      },
+    await server.callTool('browser_navigate', {
+      url: 'data:text/html,<html><title>Title</title><select><option value="foo">Foo</option><option value="bar">Bar</option></select></html>',
     });
 
-    const response = await server.send({
-      jsonrpc: '2.0',
-      id: 3,
-      method: 'tools/call',
-      params: {
-        name: 'browser_select_option',
-        arguments: {
-          element: 'Select',
-          ref: 's1e4',
-          values: ['bar'],
-        },
-      },
-    });
+    const response = await server.callTool('browser_select_option', { element: 'Select', ref: 's1e4', values: ['bar'] });
 
-    expect(response).toEqual(expect.objectContaining({
-      id: 3,
-      result: {
-        content: [{
-          type: 'text',
-          text: `Selected option in \"Select\"
+    expect(response).toEqual([
+      `Selected option in \"Select\"
 
 - Page URL: data:text/html,<html><title>Title</title><select><option value="foo">Foo</option><option value="bar">Bar</option></select></html>
 - Page Title: Title
@@ -330,45 +180,18 @@ test.describe('test browser_select_option', () => {
     - option \"Foo\" [ref=s2e5]
     - option \"Bar\" [selected] [ref=s2e6]
 \`\`\`
-`,
-        }],
-      },
-    }));
+`]);
   });
 
   test('multiple option', async ({ server }) => {
-    await server.send({
-      jsonrpc: '2.0',
-      id: 2,
-      method: 'tools/call',
-      params: {
-        name: 'browser_navigate',
-        arguments: {
-          url: 'data:text/html,<html><title>Title</title><select multiple><option value="foo">Foo</option><option value="bar">Bar</option><option value="baz">Baz</option></select></html>',
-        },
-      },
+    await server.callTool('browser_navigate', {
+      url: 'data:text/html,<html><title>Title</title><select multiple><option value="foo">Foo</option><option value="bar">Bar</option><option value="baz">Baz</option></select></html>',
     });
 
-    const response = await server.send({
-      jsonrpc: '2.0',
-      id: 3,
-      method: 'tools/call',
-      params: {
-        name: 'browser_select_option',
-        arguments: {
-          element: 'Select',
-          ref: 's1e4',
-          values: ['bar', 'baz'],
-        },
-      },
-    });
+    const response = await server.callTool('browser_select_option', { element: 'Select', ref: 's1e4', values: ['bar', 'baz'] });
 
-    expect(response).toEqual(expect.objectContaining({
-      id: 3,
-      result: {
-        content: [{
-          type: 'text',
-          text: `Selected option in \"Select\"
+    expect(response).toEqual([
+      `Selected option in \"Select\"
 
 - Page URL: data:text/html,<html><title>Title</title><select multiple><option value="foo">Foo</option><option value="bar">Bar</option><option value="baz">Baz</option></select></html>
 - Page Title: Title
@@ -380,64 +203,27 @@ test.describe('test browser_select_option', () => {
     - option \"Bar\" [selected] [ref=s2e6]
     - option \"Baz\" [selected] [ref=s2e7]
 \`\`\`
-`,
-        }],
-      },
-    }));
+`]);
   });
 });
 
 test('browser://console', async ({ server }) => {
-  await server.send({
-    jsonrpc: '2.0',
-    id: 2,
-    method: 'tools/call',
-    params: {
-      name: 'browser_navigate',
-      arguments: {
-        url: 'data:text/html,<html><script>console.log("Hello, world!");console.error("Error"); </script></html>',
-      },
-    },
+  await server.callTool('browser_navigate', {
+    url: 'data:text/html,<html><script>console.log("Hello, world!");console.error("Error"); </script></html>',
   });
-
-  const response = await server.send({
-    jsonrpc: '2.0',
-    id: 3,
-    method: 'resources/read',
-    params: {
-      uri: 'browser://console',
-    },
-  });
-  expect(response).toEqual(expect.objectContaining({
-    result: expect.objectContaining({
-      contents: [{
-        uri: 'browser://console',
-        mimeType: 'text/plain',
-        text: '[LOG] Hello, world!\n[ERROR] Error',
-      }],
-    }),
-  }));
+  expect(await server.readResource('browser://console')).toEqual([{
+    uri: 'browser://console',
+    mimeType: 'text/plain',
+    text: '[LOG] Hello, world!\n[ERROR] Error',
+  }]);
 });
 
 test('stitched aria frames', async ({ server }) => {
-  const response = await server.send({
-    jsonrpc: '2.0',
-    id: 2,
-    method: 'tools/call',
-    params: {
-      name: 'browser_navigate',
-      arguments: {
-        url: 'data:text/html,<h1>Hello</h1><iframe src="data:text/html,<h1>World</h1>"></iframe><iframe src="data:text/html,<h1>Should be invisible</h1>" style="display: none;"></iframe>',
-      },
-    },
+  const response = await server.callTool('browser_navigate', {
+    url: 'data:text/html,<h1>Hello</h1><iframe src="data:text/html,<h1>World</h1>"></iframe><iframe src="data:text/html,<h1>Should be invisible</h1>" style="display: none;"></iframe>',
   });
 
-  expect(response).toEqual(expect.objectContaining({
-    id: 2,
-    result: {
-      content: [{
-        type: 'text',
-        text: `
+  expect(response).toEqual([`
 - Page URL: data:text/html,<h1>Hello</h1><iframe src="data:text/html,<h1>World</h1>"></iframe><iframe src="data:text/html,<h1>Should be invisible</h1>" style="display: none;"></iframe>
 - Page Title: 
 - Page Snapshot
@@ -449,10 +235,8 @@ test('stitched aria frames', async ({ server }) => {
 - document [ref=f0s1e2]:
   - heading \"World\" [level=1] [ref=f0s1e4]
 \`\`\`
-`,
-      }],
-    },
-  }));
+`
+  ]);
 });
 
 test('sse transport', async () => {
