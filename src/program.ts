5a056c32185e27e91613a895cb0dd01a606fae5d
Mohammad Esmaeilirad <argo.mohrad@gmail.com>
2025-03-31 16:51:31 -0700

    Add new option to run server in qa mode

--- src/program.ts
+++ src/program.ts
@@ -38,6 +38,7 @@ program
     .option('--headless', 'Run browser in headless mode, headed by default')
     .option('--user-data-dir <path>', 'Path to the user data directory')
     .option('--vision', 'Run server that uses screenshots (Aria snapshots are used by default)')
+    .option('--qa', 'Run server to execute end to end tests on browser' )
     .option('--port <port>', 'Port to listen on for SSE transport.')
     .option('--cdp-endpoint <endpoint>', 'CDP endpoint to connect to.')
     .action(async options => {
@@ -50,6 +51,7 @@ program
         userDataDir,
         launchOptions,
         vision: !!options.vision,
+        qa: !!options.qa,
         cdpEndpoint: options.cdpEndpoint,
       }));
       setupExitWatchdog(serverList);
