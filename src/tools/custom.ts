35dd58e6711e4e05cb25191dc1b83c5876ba0a3c
Lakshman <61258323+LakshmanKishore@users.noreply.github.com>
2025-04-12 16:28:05 +0530

    Add new tool to execute JavaScript file content in the browser console
    * **src/tools/custom.ts**
      - Remove function call related code from `executeCustomScriptSchema` and `executeCustomScript`
      - Update description to reflect the change
    
    * **tests/custom.spec.ts**
      - Remove test case for executing custom JavaScript function from file

--- src/tools/custom.ts
+++ src/tools/custom.ts
@@ -6,15 +6,13 @@ import type { Tool } from './tool';
 
 const executeCustomScriptSchema = z.object({
   filePath: z.string().describe('The path to the JavaScript file to execute.'),
-  functionName: z.string().optional().describe('The name of the function to call after executing the script.'),
-  functionArgs: z.array(z.any()).optional().describe('The arguments to pass to the function.'),
 });
 
 const executeCustomScript: Tool = {
   capability: 'core',
   schema: {
     name: 'browser_custom_javascript',
-    description: 'Execute custom JavaScript from a file in the browser console and optionally call a function',
+    description: 'Execute custom JavaScript from a file in the browser console',
     inputSchema: zodToJsonSchema(executeCustomScriptSchema),
   },
 
@@ -24,12 +22,6 @@ const executeCustomScript: Tool = {
     const tab = context.currentTab();
     return await tab.run(async () => {
       await tab.page.evaluate(scriptContent);
-      if (validatedParams.functionName) {
-        await tab.page.evaluate((functionName: any, functionArgs: any) => {
-          const func = new Function(`return ${functionName}`)();
-          func(...functionArgs);
-        }, validatedParams.functionName, validatedParams.functionArgs);
-      }
     }, {
       status: `Executed custom JavaScript from file: ${validatedParams.filePath}`,
     });
--- tests/custom.spec.ts
+++ tests/custom.spec.ts
@@ -51,46 +51,6 @@ test('execute custom JavaScript from file', async ({ client }) => {
   await fs.unlink(filePath);
 });
 
-test('execute custom JavaScript function from file', async ({ client }) => {
-  const filePath = 'test-function-script.js';
-  const scriptContent = `
-    function greet(name) {
-      console.log('Hello, ' + name + '!');
-    }
-  `;
-  await fs.writeFile(filePath, scriptContent);
-
-  await client.callTool({
-    name: 'browser_navigate',
-    arguments: {
-      url: 'data:text/html,<html><title>Title</title><body></body></html>',
-    },
-  });
-
-  const response = await client.callTool({
-    name: 'browser_custom_javascript',
-    arguments: {
-      filePath,
-      functionName: 'greet',
-      functionArgs: ['Playwright'],
-    },
-  });
-
-  expect(response).toHaveTextContent('Executed custom JavaScript from file: test-function-script.js');
-
-  const resource = await client.readResource({
-    uri: 'browser://console',
-  });
-
-  expect(resource.contents).toEqual([{
-    uri: 'browser://console',
-    mimeType: 'text/plain',
-    text: '[LOG] Hello, Playwright!',
-  }]);
-
-  await fs.unlink(filePath);
-});
-
 test('execute generated JavaScript code', async ({ client }) => {
   await client.callTool({
     name: 'browser_navigate',
