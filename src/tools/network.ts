3ff647ccc3225b017800b6fc7f1c0f665eb08742
bun913 <nagasima.romance+@gmail.com>
2025-04-28 19:36:23 +0900

    chore: remove join

--- src/tools/network.ts
+++ src/tools/network.ts
@@ -36,7 +36,7 @@ const requests = defineTool({
       code: [`// <internal code to list network requests>`],
       action: async () => {
         return {
-          content: [{ type: 'text', text: log.join('\n') }]
+          content: [{ type: 'text', text: log }]
         };
       },
       captureSnapshot: false,
