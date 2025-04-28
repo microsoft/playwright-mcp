5ac87debcacb03d943eb8a815d1a74702bc90825
bun913 <nagasima.romance+@gmail.com>
2025-04-28 18:44:35 +0900

    chore: update description

--- src/tools/network.ts
+++ src/tools/network.ts
@@ -24,7 +24,7 @@ const requests = defineTool({
 
   schema: {
     name: 'browser_network_requests',
-    description: 'Returns network requests since loading the page',
+    description: 'Returns all network requests since loading the page, if the request is a xhr or fetch, it will return the request body and response body',
     inputSchema: z.object({}),
   },
 
