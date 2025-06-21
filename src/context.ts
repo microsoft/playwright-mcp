fff3d31fc9480bebd3d6d1049705f0af62620b71
Edward Upton <ed.upton@outlook.com>
2025-06-21 14:59:40 +0100

    chore: increment package version to 0.0.29-9 and simplify page creation handling in context.ts
    - Updated package version to 0.0.29-9 in package.json.
    - Simplified the page creation logic in context.ts by removing unnecessary popup handling checks.

--- src/context.ts
+++ src/context.ts
@@ -311,13 +311,9 @@ ${code.join("\n")}
   }
 
   private async _onPageCreated(page: playwright.Page) {
-    if (!this.isHandlingPopup && (await this._detectPopup(page))) {
-      await this._convertPopupToTab(page);
-    } else {
-      const tab = new Tab(this, page, (tab) => this._onPageClosed(tab));
-      this._tabs.push(tab);
-      if (!this._currentTab) this._currentTab = tab;
-    }
+    const tab = new Tab(this, page, (tab) => this._onPageClosed(tab));
+    this._tabs.push(tab);
+    if (!this._currentTab) this._currentTab = tab;
   }
 
   private _onPageClosed(tab: Tab) {
