2152e17ae103ecf4aef70c4f87d04c641d8cb683
Pavel Feldman <pavel.feldman@gmail.com>
2025-07-16 09:50:00 -0700

    chore: introduce browser_evaluate

--- tests/capabilities.spec.ts
+++ tests/capabilities.spec.ts
@@ -22,6 +22,7 @@ test('test snapshot tool list', async ({ client }) => {
     'browser_click',
     'browser_console_messages',
     'browser_drag',
+    'browser_evaluate',
     'browser_file_upload',
     'browser_generate_playwright_test',
     'browser_handle_dialog',
